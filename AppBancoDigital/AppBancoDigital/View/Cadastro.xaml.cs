77078ce31b8e617c62a0f590a62971f992c3be3c
Usuario <Usuario@CPD03-M17>
2023-05-09 11:21:18 -0300

    atualizando

--- AppBancoDigital/AppBancoDigital/View/Cadastro.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/Cadastro.xaml.cs
@@ -15,6 +15,32 @@ namespace AppBancoDigital.View
         public Cadastro()
         {
             InitializeComponent();
+            logo.Source = ImageSource.FromResource("AppBancoDigital.Imagens.Lobank1.png");
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void btn_cadastro_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                App.Current.MainPage = new NavigationPage(new View.Login());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro", ex.Message, "OK");
+            }
+        }
+
+        private async void btn_voltar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                App.Current.MainPage = new NavigationPage(new View.Login());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro", ex.Message, "OK");
+            }
         }
     }
 }

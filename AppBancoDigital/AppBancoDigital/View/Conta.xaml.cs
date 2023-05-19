e730562732806f926bd1bec457cee5f513eac9c0
Usuario <Usuario@CPD02-M02>
2023-05-19 09:43:21 -0300

    atualizando

--- AppBancoDigital/AppBancoDigital/View/Conta.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/Conta.xaml.cs
@@ -12,16 +12,35 @@ namespace AppBancoDigital.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Conta : ContentPage
     {
+        bool txt_saldo = false;
         public Conta()
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
             logo.Source = ImageSource.FromResource("AppBancoDigital.Imagens.Lobank2.png");
+            btnolho.Source = ImageSource.FromResource("AppBancoDigital.Imagens.olho_fechado.png");
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void btnolho_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                if (!txt_saldo) 
+                {
+                    txt_saldo = true;
+                    btnolho.Source = ImageSource.FromResource("AppBancoDigital.Imagens.olho_fechado.png");
+                }
+                else
+                {
+                    txt_saldo = false;
+                    btnolho.Source = ImageSource.FromResource("AppBancoDigital.Imagens.olho_aberto.png");
+                }
 
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ocorreu um erro", ex.Message, "OK");
+            }
         }
     }
 }
